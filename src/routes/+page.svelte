8e8606263632b0f578f362eedd28618f2da4704a
benny <bennycortese@gmail.com>
2023-12-20 02:07:22 -0500

    Moved video on the page and coupled some buttons for question answering with it

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,44 @@
 <script>
     import Youtube from './Youtube.svelte';
     import VideoPlayer from './VideoPlayer.svelte';
-  </script> 
-<style>
-</style>
+  </script>
   
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the docs</p>
-
-<input type="text" placeholder="Type here" class="input w-full max-w-xs" />
-<VideoPlayer 
-  videoSrc="/video5elvaGrc37U.mp4"
-/>
-<input type="text" placeholder="Type here" class="input w-full max-w-xs" />
+  <style>
+    .video-container {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      min-height: (100vh - 40px); /* Full height of the viewport */
+      padding: 0px;
+    }
+  
+    .video-player-wrapper {
+      width: 80%; /* Adjust this value as needed */
+      margin: 0 auto; /* This will center the video player if it has a width less than 100% */
+    }
+  
+    /* Style the input to match your design */
+    .input {
+      margin-bottom: 0px; /* Spacing between the input and video */
+      width: 100%; /* Full width of its container */
+      max-width: 400px; /* Maximum width */
+    }
+  </style>
+  
+  <div class="video-container">
+    <h1>Welcome to SvelteKit</h1>
+    <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the docs</p>
+    <div class="video-player-wrapper">
+      <VideoPlayer 
+        videoSrc="/video5elvaGrc37U.mp4"
+      />
+      <div class="buttons-container">
+        <button class="btn btn-primary">A</button>
+        <button class="btn btn-primary">B</button>
+        <button class="btn btn-primary">C</button>
+        <button class="btn btn-primary">D</button>
+      </div>
+    </div>
+  </div>
+  
--- src/routes/VideoPlayer.svelte
+++ src/routes/VideoPlayer.svelte
@@ -1,8 +1,26 @@
 <script>
     export let videoSrc = ''; // URL of the MP4 file
-  </script>
-  
-  <video src={videoSrc} controls width="640" height="360">
-    Your browser does not support the video tag.
-  </video>
-  
+</script>
+
+<style>
+    video {
+        width: 100%; /* Make video width responsive to the container */
+        max-width: 640px; /* Maximum video width */
+        height: auto; /* Maintain aspect ratio */
+        max-height: 360px; /* Maximum video height */
+    }
+
+    .video-container {
+        display: flex;
+        justify-content: flex-start; /* Aligns content to the top */
+        align-items: center;
+        padding-top: 20px; /* Adjust this value as needed */
+        padding-bottom: 20px; /* Adds some padding at the bottom */
+    }
+</style>
+
+<div class="video-container">
+    <video src={videoSrc} controls>
+        Your browser does not support the video tag.
+    </video>
+</div>
